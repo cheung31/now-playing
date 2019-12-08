d29099d1eaf4ccac06ce18f8ce23d75f77f14069
Ryan Cheung <cheung31@gmail.com>
2019-12-08 15:48:08 -0800

    react-window integration to NowPlayingList

--- src/client/pages/Home/NowPlayingList.js
+++ src/client/pages/Home/NowPlayingList.js
@@ -1,7 +1,14 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { FixedSizeList as List } from 'react-window';
+
+const Row = ({ data, index, style }) => {
+  let movie = data[index];
+  return <div style={style}>{movie.original_title}</div>;
+};
 
 function NowPlayingList() {
+  const [listHeight, setListHeight] = useState(0);
   const [moviesList, setMoviesList] = useState([]);
 
   useEffect(() => {
@@ -11,27 +18,35 @@ function NowPlayingList() {
       });
   }, []);
 
+  useEffect(() => {
+    function handleWindowResize() {
+      setListHeight(window.innerHeight - 120);
+    }
+
+    setListHeight(window.innerHeight - 120);
+    window.addEventListener('resize', handleWindowResize);
+
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
   if (!moviesList.length) {
     return <p>Loading...</p>;
   }
 
-  console.log(moviesList);
   return (
-    <div>
-      {
-        moviesList.map(movie => {
-          return <p key={movie.id}>{movie.original_title}</p>
-        })
-      }
-    </div>
+    <List
+      height={listHeight}
+      itemCount={moviesList.length}
+      itemData={moviesList}
+      itemSize={35}
+      width='100%'
+    >
+      {Row}
+    </List>
   );
-}
-
-function Blah() {
-  if (true) {
-    return <h3>wtf</h3>
-  }
-  return <h3>Blah blah</h3>
 }
 
 export default NowPlayingList;
